0d4fdd8c9e6a9bdb8402e0b1d69c24a12b6d7d31
“Natalia <natuta2004@gmail.com>
2024-05-11 12:57:07 -0400

    Fix cache files in build folder

--- WebApplication1/WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0875c57fae8b92cbc69088027c6c5572848a3fce")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+39087b376ae9decc2e434a22047cd6f59506a7e1")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
